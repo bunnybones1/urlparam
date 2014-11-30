0b481f0c8327d0125beb442b10a93d9187edd26c
Tomasz Dysinski <oz@bunnybones.com>
2014-11-30 00:24:53 -0500

    fixed redundant reminders, can be deactived easily

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var URLParams = {
+	active : true
+};
 var reminders = [];
 var reminderGiven = false;
 function remind() {
@@ -7,12 +10,14 @@ function remind() {
 };
 
 function getParam(name) {
-	reminders.push(name);
-	reminderGiven = false;
-	setTimeout(remind, 2000);
+	if(!URLParams.active) return;
+	if(reminders.indexOf(name) == -1) {
+		reminders.push(name);
+		reminderGiven = false;
+		setTimeout(remind, 2000);
+	}
 	var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
 	var results = regex.exec(window.location.href);
-	
 	if (results==null){
 	   return undefined;
 	} else if(results[1] == "true"){
@@ -26,6 +31,6 @@ function getParam(name) {
 	}
 }
 
-module.exports = {
-	getParam : getParam
-}
+URLParams.getParam = getParam;
+
+module.exports = URLParams;
--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 var urlParams = require('./');
+var domQuickText = require('dom-quick-text');
 
 var test = urlParams.getParam('test');
 var test2 = urlParams.getParam('test2');
+var redundancyTest = urlParams.getParam('test2');
 
-console.log(test, test2);
+domQuickText('test:' + test);
+domQuickText('test2:' + test2);
+
+console.log(test, test2, redundancyTest);
+
