33c9334600c725f11f482a681abccac3a2a85ac0
Tomasz Dysinski <tom@jam3.com>
2016-11-11 10:35:18 -0500

    allow numbers, not just integers

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function getURLParam(name, defaultValue) {
 	var results = regex.exec(window.location.href);
 	if (results==null){
 	   return defaultValue;
-	} else if(results[1] == "true"){
+	} else if(results[1] === "true"){
 		return true;
-	} else if(results[1] == "false"){
+	} else if(results[1] === "false"){
 		return false;
 	} else if(!isNaN(results[1])){
-		return parseInt(results[1]);
+		return Number(results[1]);
 	} else {
 		return results[1] || defaultValue || false;
 	}
