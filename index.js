ee782fc3328f1e8a9afbd9c1714097a4c9fab233
Tomasz Dysinski <oz@bunnybones.com>
2017-10-22 20:43:12 -0400

    adding new feature to expose a urlparam by forcing it into the location url with a simple argument

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-var reminders = [];
+var paramKeys = [];
+var paramValues = [];
+var wasProvided = [];
+var forcedFlags = [];
 var reminderGiven = false;
 function remind() {
 	if(reminderGiven) return;
 	reminderGiven = true;
-	console.log("URL PARAMS AVAILABLE: " + reminders.join(", "));
+	console.log("URL PARAMS AVAILABLE: " + paramKeys.join(", "));
+	var shouldRefresh = forcedFlags.some(function(forced, i) {
+		return forced && !wasProvided[i];
+	});
+	if(shouldRefresh) {
+		var keyPairs = paramKeys.map(function(key, i) {
+			return forcedFlags[i] ? key + '=' + paramValues[i] : null;
+		});
+		keyPairs = keyPairs.filter(function(v) { return v; });
+		var keyPairString = keyPairs.join('&');
+		var newLoc = window.location.origin + '?' + keyPairString;
+
+		window.location = newLoc;
+	}
 };
 
-function getURLParam(name, defaultValue) {
+function getURLParam(name, defaultValue, forcedExpose) {
 	if(!getURLParam.active) return defaultValue || null;
-	if(reminders.indexOf(name) == -1) {
-		reminders.push(name);
+	var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+	var results = regex.exec(window.location.href);
+	var result = false;
+	var provided = true;
+	if (results == null){
+	   result = defaultValue;
+	   provided = false;
+	} else if(results[1] === "true"){
+		result = true;
+	} else if(results[1] === "false"){
+		result = false;
+	} else if(!isNaN(results[1])){
+		result = Number(results[1]);
+	} else {
+		result = results[1] || defaultValue || false;
+	}
+	if(paramKeys.indexOf(name) == -1) {
+		paramKeys.push(name);
+		paramValues.push(result);
+		forcedFlags.push(!!forcedExpose);
+		wasProvided.push(provided);
 		reminderGiven = false;
 		setTimeout(remind, 2000);
 	}
-	var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-	var results = regex.exec(window.location.href);
-	if (results==null){
-	   return defaultValue;
-	} else if(results[1] === "true"){
-		return true;
-	} else if(results[1] === "false"){
-		return false;
-	} else if(!isNaN(results[1])){
-		return Number(results[1]);
-	} else {
-		return results[1] || defaultValue || false;
-	}
+
+	return result; 
 }
 
 getURLParam.active = true;
--- test.js
+++ test.js
@@ -6,9 +6,11 @@ var domQuickText = require('dom-quick-text');
 var test = urlParam('test', 'default');
 var test2 = urlParam('test2', 1000);
 var redundancyTest = urlParam('test2', 10);
+var test3Force = urlParam('test3', 2000, true);
 
-domQuickText('test:' + test);
-domQuickText('test2:' + test2);
+domQuickText('test: ' + test);
+domQuickText('test2: ' + test2);
+domQuickText('test3Force: ' + test3Force);
 
 console.log(test, test2, redundancyTest);
 
